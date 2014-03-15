3e21562778692f3186cb8d51ac98e12523de4ec9
sharedRoutine <appleidmail@aol.com>
2014-03-15 16:47:39 +0100

    Replaced with CPDistributedNotificationCenter

--- CPDistributedMessagingCenter.h
+++ CPDistributedMessagingCenter.h
@@ -20,10 +20,22 @@
 	NSMutableDictionary* _callouts;
 }
 +(CPDistributedMessagingCenter*)centerNamed:(NSString*)serverName;
+-(id)_initWithServerName:(NSString*)serverName;
+// inherited: -(void)dealloc;
+-(NSString*)name;
+-(unsigned)_sendPort;
+-(void)_serverPortInvalidated;
 -(BOOL)sendMessageName:(NSString*)name userInfo:(NSDictionary*)info;
+-(NSDictionary*)sendMessageAndReceiveReplyName:(NSString*)name userInfo:(NSDictionary*)info;
+-(NSDictionary*)sendMessageAndReceiveReplyName:(NSString*)name userInfo:(NSDictionary*)info error:(NSError**)error;
+-(void)sendMessageAndReceiveReplyName:(NSString*)name userInfo:(NSDictionary*)info toTarget:(id)target selector:(SEL)selector context:(void*)context;
+-(BOOL)_sendMessage:(id)message userInfo:(id)info receiveReply:(id*)reply error:(id*)error toTarget:(id)target selector:(SEL)selector context:(void*)context;
+-(BOOL)_sendMessage:(id)message userInfoData:(id)data oolKey:(id)key oolData:(id)data4 receiveReply:(id*)reply error:(id*)error;
 -(void)runServerOnCurrentThread;
 -(void)runServerOnCurrentThreadProtectedByEntitlement:(id)entitlement;
 -(void)stopServer;
 -(void)registerForMessageName:(NSString*)messageName target:(id)target selector:(SEL)selector;
+-(void)unregisterForMessageName:(NSString*)messageName;
+-(id)_requiredEntitlement;
 @end
 
