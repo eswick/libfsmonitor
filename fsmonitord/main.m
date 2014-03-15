917779587aa1fc1a1513c345114a55a93c890846
sharedRoutine <appleidmail@aol.com>
2014-03-15 16:48:58 +0100

    Added CPDistributedMessagingCenter and rocketbootstrap

--- fsmonitord/main.m
+++ fsmonitord/main.m
@@ -9,7 +9,6 @@
 #include <pwd.h>
 #include <grp.h>
 #import <rocketbootstrap.h>
-
 #import "fsevents.h"
 #import "../CPDistributedMessagingCenter.h"
 
@@ -52,7 +51,7 @@ int main(int argc, char **argv){
 	int32_t arg_id;
 	int     fd, clonefd = -1;
 	int     i, eoff, off, ret;
-    
+
 	kfs_event_arg_t *kea;
 	struct           fsevent_clone_args fca;
 	char             buffer[FSEVENT_BUFSIZ];
@@ -70,62 +69,62 @@ int main(int argc, char **argv){
 		FSE_REPORT,  // FSE_XATTR_MODIFIED,
 		FSE_REPORT,  // FSE_XATTR_REMOVED,
 	};
-    
+
 	notifier = [CPDistributedMessagingCenter centerNamed:@"com.eswick.libfsmonitor"];
 	rocketbootstrap_distributedmessagingcenter_apply(notifier);
-    
+
 	if (geteuid() != 0) {
 		NSLog(@"Error: %s must be run as root.", argv[0]);
 		exit(1);
 	}
-    
+
 	setbuf(stdout, NULL);
-    
+
 	if ((fd = open(DEV_FSEVENTS, O_RDONLY)) < 0) {
 		perror("open");
 		exit(1);
 	}
-    
+
 	fca.event_list = (int8_t *)event_list;
 	fca.num_events = sizeof(event_list)/sizeof(int8_t);
 	fca.event_queue_depth = EVENT_QUEUE_SIZE;
-	fca.fd = &clonefd;
+	fca.fd = &clonefd; 
 	if ((ret = ioctl(fd, FSEVENTS_CLONE, (char *)&fca)) < 0) {
 		perror("ioctl");
 		close(fd);
 		exit(1);
 	}
-    
+
 	close(fd);
 	//printf("fsevents device cloned (fd %d)\nfslogger ready\n", clonefd);
-    
+
 	if ((ret = ioctl(clonefd, FSEVENTS_WANT_EXTENDED_INFO, NULL)) < 0) {
 		perror("ioctl");
 		close(clonefd);
 		exit(1);
 	}
-    
+
 	while (1) { // event processing loop
-        
+
 		ret = read(clonefd, buffer, FSEVENT_BUFSIZ);
-        
+
 		off = 0;
-        
+
 		while (off < ret) { // process one or more events received
-            
+
 			struct kfs_event *kfse = (struct kfs_event *)((char *)buffer + off);
-            
+
 			off += sizeof(int32_t) + sizeof(pid_t); // type + pid
-            
+
 			if (kfse->type == FSE_EVENTS_DROPPED) { // special event
 				NSLog(@"Process %d dropped events.", kfse->pid);
 				off += sizeof(u_int16_t); // FSE_ARG_DONE: sizeof(type)
 				continue;
 			}
-            
+
 			int32_t atype = kfse->type & FSE_TYPE_MASK;
 			uint32_t aflags = FSE_GET_FLAGS(kfse->type);
-            
+
 			if ((atype < FSE_MAX_EVENTS) && (atype >= -1)) { //atype = Event Type
 				if (aflags & FSE_COMBINED_EVENTS) {
 					//Combined events
@@ -137,94 +136,95 @@ int main(int argc, char **argv){
 				NSLog(@"This may be a program bug (type = %d).", atype);
 				exit(1);
 			}
-            
+
 			NSMutableArray *arguments = [[NSMutableArray alloc] init];
-            
-			kea = kfse->args;
+
+			kea = kfse->args; 
 			i = 0;
-            
+
 			while (off < ret) {// process arguments
 				i++;
-                
+
 				if (kea->type == FSE_ARG_DONE) { // no more arguments
 					off += sizeof(u_int16_t);
 					break;
 				}
-                
+
 				eoff = sizeof(kea->type) + sizeof(kea->len) + kea->len;
 				off += eoff;
-                
+
 				arg_id = (kea->type > FSE_MAX_ARGS) ? 0 : kea->type;
-                
+
 				switch (kea->type) { // handle based on argument type
-                        
+
 					case FSE_ARG_VNODE:  // a vnode (string) pointer
 						is_fse_arg_vnode = 1;
 						[arguments addObject:[NSString stringWithUTF8String:(char *)&(kea->data.vp)]];
 						break;
-                        
+
 					case FSE_ARG_STRING: // a string pointer
 						[arguments addObject:[NSString stringWithUTF8String:(char *)&(kea->data.str)]];
 						break;
-                        
+
 					case FSE_ARG_INT32:
 						[arguments addObject:@(kea->data.int32)];
 						break;
-                        
+
 					case FSE_ARG_RAW: // a void pointer
 						//Not implemented
 						break;
-                        
+
 					case FSE_ARG_INO: // an inode number
 						[arguments addObject:@((uint32_t)kea->data.ino)];
 						break;
-                        
+
 					case FSE_ARG_UID: // a user ID
 						[arguments addObject:@(kea->data.uid)];
 						break;
-                        
+
 					case FSE_ARG_DEV: // a file system ID or a device number
 						[arguments addObject:@(kea->data.dev)];
 						break;
-                        
+
 					case FSE_ARG_MODE: // a combination of file mode and file type
 						[arguments addObject:@(kea->data.mode)];
 						break;
-                        
+
 					case FSE_ARG_GID: // a group ID
 						[arguments addObject:@(kea->data.gid)];
 						break;
 					case FSE_ARG_INT64: // timestamp
 						[arguments addObject:@(kea->data.timestamp)];
 						break;
-                        
+
 					default:
 						NSLog(@"such argument. so unknown. wow.");
 						NSLog(@"(Unknown event argument)");
 						[arguments addObject:@"unknown"];
 						break;
 				}
-                
+
 				kea = (kfs_event_arg_t *)((char *)kea + eoff); // next
 			} // for each argument
 			handleEvent(kfse->pid, kfse->type, arguments);
 			[arguments release];
 		} // for each event
 	} // forever
-    
+
 	close(clonefd);
-    
+
 	exit(0);
 }
 
 
-void handleEvent(pid_t pid, int32_t type, NSArray *arguments){
-	NSMutableDictionary *event = [NSMutableDictionary new];
+void handleEvent(pid_t pid, int32_t type, NSArray *arguments) {
     
+	NSMutableDictionary *event = [NSMutableDictionary new];
+
 	[event setObject:@(type) forKey:@"TYPE"];
 	[event setObject:@(pid) forKey:@"PID"];
 	[event setObject:[arguments objectAtIndex:[arguments count] - 1] forKey:@"TIMESTAMP"];
-    
+
 	switch(type){
 		case FSE_XATTR_REMOVED:
 		case FSE_FINDER_INFO_CHANGED:
@@ -244,9 +244,10 @@ void handleEvent(pid_t pid, int32_t type, NSArray *arguments){
 			[event setObject:[arguments objectAtIndex:5] forKey:@"GID"];
 			break;
 	}
-    
-	[notifier sendMessageName:@"FSMonitorInfoMessage" userInfo:event];
-    
+
+    NSDictionary *finalEvent = [[NSDictionary alloc] initWithDictionary:event];
+	[notifier sendMessageName:@"FSMonitorInfoMessage" userInfo:finalEvent];
 	[event release];
+    [finalEvent release];
 }
 
